448db3e6a6e09f7071dfd8122cdc4cd444d3b9b4
konstantin <kostyani4y@yandex.ru>
2026-07-08 11:47:12 +0300

    вставлена, настроена задача 16.1.7

--- practice_16_1/x16_1_7_Zadacha/tests_16_1_7/test_task.py
+++ practice_16_1/x16_1_7_Zadacha/tests_16_1_7/test_task.py
@@ -1,10 +1,7 @@
 import datetime
-from pyexpat.errors import messages
-
-from black import assert_equivalent
-# from task import Task
 from practice_16_1.x16_1_7_Zadacha.src_16_1_7.task import Task
 
+# from task import Task
 # from src_16_1_7.task import Task
 
 # ================================
