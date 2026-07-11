2be24f30ae4d4e2c98f8b26bf4e2cb0e1d6ef6b0
konstantin <kostyani4y@yandex.ru>
2026-07-11 19:35:45 +0300

    задача

---
+++ practice_16_2/x16_1_7_Zadacha/tests_16_1_7/test_periodic_task.py
@@ -0,0 +1,26 @@
+import pytest
+from practice_16_1.x16_1_7_Zadacha.tests_16_1_7.conftest import task_periodic1
+
+# ================================
+# запуск тестов
+# poetry run pytest practice_16_1/x16_1_7_Zadacha/tests_16_1_7/test_periodic_task.py
+# ================================
+
+def test_periodic_task_init(task_periodic1):
+    assert task_periodic1.name == 'Купить огурцы'
+    assert task_periodic1.description == 'Купить огурцы для салата'
+    assert task_periodic1.start_data == '01.01.2026'
+    assert task_periodic1.end_data == '01.01.2026'
+    assert task_periodic1.status == 'Ожидает старта'
+    assert task_periodic1.created_at == '12.06.2026'
+    assert task_periodic1.frequency == 'Ежедневная'
+
+def test_periodic_task_add(task_periodic1, task_periodic2):
+    """ Проверяем что сложение происходит успешно"""
+    assert task_periodic1 + task_periodic2 == 120
+
+def test_periodic_task_add_error(task_periodic1, task_periodic2):
+    """ Проверяем что сложение происходит с ошибкой"""
+    # result = task_periodic1 + 1   # проверяем в не контекстного менеджера возбуждается ошибка
+    with pytest.raises(TypeError):
+        result = task_periodic1 + 1
