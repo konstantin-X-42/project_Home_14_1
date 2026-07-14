0600e2e96f4d039ac028542510508ce24c8ce24f
konstantin <kostyani4y@yandex.ru>
2026-07-15 00:06:44 +0300

    удалил папку practice

--- practice_17_1/x17_1_11_Zadacha/src_17_1_11/base_task.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class BaseTask(ABC):
-
-    @classmethod
-    @abstractmethod
-    def new_task(cls, *args, **kwargs):
-        pass
--- practice_17_1/x17_1_11_Zadacha/src_17_1_11/exceptions.py
+++
@@ -1,5 +0,0 @@
-class ZeroRunTimeTask(Exception):
-    """Кастомская ошибка возбуждается если пользователю добавляется задача с нулевым временем"""
-
-    def __init__(self, message=None):
-        super().__init__(message)
